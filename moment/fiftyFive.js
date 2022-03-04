c7bec7c4a2f1cbffc928e1fa63886e7d50ad2819
apsingh001 <64854893+apsingh001@users.noreply.github.com>
2022-03-04 15:48:43 +0530

    Add prompt-sync in myBirthday and fiftyFive

--- moment/fiftyFive.js
+++ moment/fiftyFive.js
@@ -1,7 +1,10 @@
+const prompt = require('prompt-sync')();
 const moment=require('moment');
-function FiftyFive(a){
-    let b=a.add(55,'days');
+function FiftyFive(a,x){
+    let b=a.add(x,'days');
     return b;
 }
-let a=moment();
-console.log(FiftyFive(a).format("DD-MM-YY"));
+let a=prompt("Enter the date (in YYYY-MM-DD format): ");
+a=moment(a);
+let x=prompt("Enter the days you want to add: ")
+console.log("The date will be: "+""+FiftyFive(a,x).format("DD-MM-YY"));
--- moment/myBirthday.js
+++ moment/myBirthday.js
@@ -1,18 +1,21 @@
+const prompt = require('prompt-sync')();
 const moment=require('moment');
 
 function MyBirthday(a){
-    let b=moment().diff(a,'years');
+    
+    let b=moment().diff(a,'years')+1;
     let x=a.add(b,'years');
-    if(x.isSame(moment().format("YYYY-MM-DD"))){
+    let dayys=x.diff(moment(),'days')+1;
+    if(x.month()===moment().month() && x.date()===moment().date()){
         return "Happy Birthday!";
     }
-    else{
-        x=x.add(1,'years');
-        return x.diff(moment(),'days')+1;
+    if(dayys===1){
+        return dayys+" Day is left for your Birthday!";
     }
+    return dayys+" Days are left for your Birthday!";
 }
-let a="2000-03-04";
-a=moment(a);
 
+let a=prompt("Enter Your Date of Birth (in YYYY-MM-DD format): ");
+a=moment(a);
 
 console.log(MyBirthday(a));
